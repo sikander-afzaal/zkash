a16535e534a8de5e8cd475c3e88fee92b713a788
sikander-afzaal <sikander.aaa@gmail.com>
2023-04-09 20:12:50 +0500

    update all done

--- src/pages/Token/Index.jsx
+++ src/pages/Token/Index.jsx
@@ -4,7 +4,21 @@ import Button from "../../components/Button";
 const Token = () => {
   const { pathname } = useLocation();
   return (
-    <div className="wrapper mt-[160px] pb-20 xl:mt-[200px]">
+    <div className="wrapper mt-[160px] pb-20 xl:mt-[200px] isolate relative">
+      {pathname === "/token/personal" && (
+        <>
+          <img
+            src="/coin2.png"
+            className="absolute left-[1%] bottom-2 lg:bottom-[20%] -z-10"
+            alt=""
+          />
+          <img
+            src="/coin1.png"
+            className="absolute right-[1%] lg:block hidden top-[0%] -z-10"
+            alt=""
+          />
+        </>
+      )}
       <div className="contain flex-col gap-10 justify-center items-center">
         <div className="flex sm:flex-row flex-col w-full justify-center items-center gap-7">
           <Link to={"/token"} className="w-full sm:w-auto">
